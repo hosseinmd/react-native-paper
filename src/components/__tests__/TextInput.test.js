5122389673e2be6e13cb270fc8c211fad36cdf74
Jan Jaworski <jaworek3211@gmail.com>
2022-09-06 10:59:07 +0200

    fix: eslint errors

--- src/components/__tests__/TextInput.test.js
+++ src/components/__tests__/TextInput.test.js
@@ -51,7 +51,7 @@ it('correctly renders left-side icon adornment, and right-side affix adornment',
   expect(toJSON()).toMatchSnapshot();
 });
 
-it('correctly renders left-side icon adornment, and right-side affix adornment', () => {
+it('correctly renders left-side affix adornment, and right-side icon adornment', () => {
   const { getByText, getByTestId, toJSON } = render(
     <TextInput
       accessibilityLabel="Text input field"
